0cad749f4141b6846c1cc4f7600a5ecc6072072e
QuantumSoham <quantumsc03@gmail.com>
2025-12-09 14:06:52 +0530

    added documentation for api gateway

--- security-api-gateway/src/main/java/com/flightapp/apigateway/controller/AuthController.java
+++ security-api-gateway/src/main/java/com/flightapp/apigateway/controller/AuthController.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+//a simple controller to handle user authentication and role based login
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -32,7 +32,7 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-    // simple test
+    // simple test to see if api server responds
     @GetMapping("/ping")
     public String ping() {
         return "Auth OK";
